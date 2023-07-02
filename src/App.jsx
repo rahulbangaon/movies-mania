05c8c27051079f87a63ff266c8bd5931269e8477
Rahul Dhaka Bangaon <rahulbangaon@yahoo.com>
2023-07-02 13:25:48 +0530

    initial commit- project setup and header, hero and footer section completed

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/pageNotFound/PageNotFound";
+
+import { fetchDataFromApi } from "./utils/api";
+import { getApiConfiguration } from "./store/homeSlice";
+import { useDispatch } from "react-redux";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    apiConfiguration();
+  }, []);
+
+  const apiConfiguration = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
+    });
+  };
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import './style.scss';
+
+import HeroBanner from './heroBanner/HeroBanner';
+
+const Home = () => {
+  return (
+    <div className='homePage'>
+      <HeroBanner />
+      <div style={{height: 1000}}></div>
+    </div>
+    
+  )
+};
+
+export default Home;
